79905425cd6421fcd65febc63e120ac2811a04c9
GuilhermeNC <guilherme.caires.santos@gmail.com>
2023-07-19 21:07:34 -0300

    Add alert for login
    I add alert for make login, if username igual password "Bem vindo ao site" else "Usuário ou senha errados"

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-document.getElementById('main-button')
+const button = document.getElementById("main-button");
+const email = document.getElementsByTagName("input")[0];
+const password = document.getElementsByClassName("inputpassword")[0];
 
+function login() {
+    if (email.value === password.value) {
+        alert("Bem-vindo ao site");
+    } else {
+        alert("Usuário ou senha incorreta");
+    }
+}
 
-
-
-
-addEventListener('alert', login)
+button.addEventListener("click", login);
